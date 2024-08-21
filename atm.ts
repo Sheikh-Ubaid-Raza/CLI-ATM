b281f7fb30b8ada151ed02b87271de786655a63d
unknown <shekhubaidraza76@gmail.com>
2024-08-21 11:43:54 +0500

    This is the first commit

---
+++ atm.ts
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance = 50000;
+let myPin = 1234;
+
+let pinAnswer = await inquirer.prompt([{
+    name:"pin",
+    message:"Enter Your Pin Number : ",
+    type:"number"
+}]);
+
+if (pinAnswer.pin === myPin) {
+    console.log("Correct Pin Code!");
+
+    let operationAns = await inquirer.prompt([{
+        name:"operation",
+        message:"Select One Operation!",
+        type:"list",
+        choices:["Withdraw","Check Balance"]
+    }]);
+
+    if (operationAns.operation === "Withdraw") {
+        let amountAns = await inquirer.prompt([{
+            name:"amount",
+            message:"Enter Your Withdraw Amount: ",
+            type:"number"
+        }]);
+
+        myBalance -= amountAns.amount;
+        console.log(`Your Remaining Balance is ${myBalance}`);
+        
+    }else if(operationAns.operation === "Check Balance"){
+        console.log(`Your Current Balance is ${myBalance}`);
+    }
+}else{
+    console.log("Incorrect Pin Code!");
+}
